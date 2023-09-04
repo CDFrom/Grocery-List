0a5324dbff9167ce15d67db2cf958a7f49a977d3
Colin From <cdfrom481@gmail.com>
2023-09-04 15:08:01 -0600

    Set container height to svg height

--- src/assets/Arrow.jsx
+++ src/assets/Arrow.jsx
@@ -1,6 +1,6 @@
 const Arrow = (props) => {
   return (
-    <div {...props}>
+    <div {...props} style={{ height: "16px" }}>
       <svg height='16' width='16'>
         <path d='M0 0 L16 8 L0 16 Z' />
       </svg>
