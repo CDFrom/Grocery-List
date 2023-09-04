c586b04529f8c15dbcb1c24943ead7ad3c7d082b
Colin From <cdfrom481@gmail.com>
2023-09-04 10:52:10 -0600

    Add itemList for dynamic generation of content

--- src/components/ListItems/Store.jsx
+++ src/components/ListItems/Store.jsx
@@ -1,13 +1,19 @@
 import Card from "../UI/Card/Card";
+import Item from "./Item";
 
 import classes from "./Store.module.css";
 
 const Store = (props) => {
   const store = props.store;
 
+  const itemList = store.items.map((item) => {
+    return <Item key={item} item={item} />;
+  });
+
   return (
     <Card className={classes.store}>
       <h1 className={classes["store__title"]}>{store.name}</h1>
+      {itemList}
     </Card>
   );
 };
