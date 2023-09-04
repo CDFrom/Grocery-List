9411f799893127d0fae7ae20781e0236efeb97ea
Colin From <cdfrom481@gmail.com>
2023-09-04 08:38:51 -0600

    Add state for showing GetText in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
+import GetText from "./components/AddField/GetText";
+
 import "./App.css";
 
 const App = () => {
-  return <>Let's begin</>;
+  const [isInputOpen, setIsInputOpen] = useState(false);
+
+  const openInput = () => {
+    setIsInputOpen(true);
+  };
+
+  const closeInput = () => {
+    setTimeout(() => {
+      setIsInputOpen(false);
+    }, 300);
+  };
+
+  return <>{isInputOpen && <GetText onClose={closeInput} />}</>;
 };
 
 export default App;
