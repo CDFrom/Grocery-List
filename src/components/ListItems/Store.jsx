d7efc18b313622af4d12e4aaa5a23a6d558637b7
Colin From <cdfrom481@gmail.com>
2023-09-04 09:55:31 -0600

    Add store as a constant, adjust h1 content

--- src/components/ListItems/Store.jsx
+++ src/components/ListItems/Store.jsx
@@ -3,9 +3,11 @@ import Card from "../UI/Card/Card";
 import classes from "./Store.module.css";
 
 const Store = (props) => {
+  const store = props.store;
+
   return (
     <Card className={classes.store}>
-      <h1 className={classes["store__title"]}>{props.store}</h1>
+      <h1 className={classes["store__title"]}>{store.name}</h1>
     </Card>
   );
 };
