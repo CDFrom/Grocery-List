3cc837bf0e91b1f469cbd1365dbb20058fb302ef
Colin From <cdfrom481@gmail.com>
2023-09-04 09:43:59 -0600

    Fill in Store.jsx and add styling

--- src/components/ListItems/Store.jsx
+++ src/components/ListItems/Store.jsx
@@ -1,5 +1,13 @@
+import Card from "../UI/Card/Card";
+
 import classes from "./Store.module.css";
 
-const Store = (props) => {};
+const Store = (props) => {
+  return (
+    <Card className={classes.store}>
+      <h1 className={classes["store__title"]}>{props.store}</h1>
+    </Card>
+  );
+};
 
 export default Store;
