6c91450db55d9c9d1487c958c23154f46f699516
Colin From <cdfrom481@gmail.com>
2023-09-04 08:38:29 -0600

    Wrap GetText with a Modal

--- src/components/AddField/GetText.jsx
+++ src/components/AddField/GetText.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
+import Modal from "../UI/Modal/Modal";
 
 import classes from "./GetText.module.css";
 
@@ -18,15 +19,17 @@ const GetText = (props) => {
   };
 
   return (
-    <Card className={classes.form}>
-      <form onSubmit={submitHandler}>
-        <div>
-          <label htmlFor='userInput'>{props.inputFor}: </label>
-          <input type='text' name='userInput' id='userInput' ref={inputRef} />
-        </div>
-        <Button>Add</Button>
-      </form>
-    </Card>
+    <Modal onClose={props.onClose}>
+      <Card className={classes.form}>
+        <form onSubmit={submitHandler}>
+          <div>
+            <label htmlFor='userInput'>{props.inputFor}: </label>
+            <input type='text' name='userInput' id='userInput' ref={inputRef} />
+          </div>
+          <Button>Add</Button>
+        </form>
+      </Card>
+    </Modal>
   );
 };
 
