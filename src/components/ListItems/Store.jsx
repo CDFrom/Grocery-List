16c62d7d1fed9decb4b384ba200ebd3b2ac3ebeb
Colin From <cdfrom481@gmail.com>
2023-09-04 13:14:28 -0600

    Add form for adding an item

--- src/components/ListItems/Store.jsx
+++ src/components/ListItems/Store.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
+import GetText from "../AddField/GetText";
 import Item from "./Item";
 
 import classes from "./Store.module.css";
 
 const Store = (props) => {
   const store = props.store;
+  const [isAddItemOpen, setIsAddItemOpen] = useState(false);
+
+  const addItemHandler = () => {
+    setIsAddItemOpen(true);
+  };
 
   const removeItemHandler = (itemToRemove) => {
     const updatedItemList = store.items.filter((item) => item !== itemToRemove);
     props.onUpdateStore(store.name, updatedItemList);
+  };
+
+  const closeAddItemHandler = () => {
+    setTimeout(() => {
+      setIsAddItemOpen(false);
+    }, 300);
   };
 
   const itemList = store.items.map((item) => {
@@ -17,13 +31,18 @@ const Store = (props) => {
   });
 
   return (
-    <Card className={classes.store}>
-      <div className={classes["store__header"]}>
-        <h1 className={classes["store__title"]}>{store.name}</h1>
-        <Button>+</Button>
-      </div>
-      {itemList}
-    </Card>
+    <>
+      {isAddItemOpen && (
+        <GetText inputFor='Item' onClose={closeAddItemHandler} />
+      )}
+      <Card className={classes.store}>
+        <div className={classes["store__header"]}>
+          <h1 className={classes["store__title"]}>{store.name}</h1>
+          <Button onClick={addItemHandler}>+</Button>
+        </div>
+        {itemList}
+      </Card>
+    </>
   );
 };
 
