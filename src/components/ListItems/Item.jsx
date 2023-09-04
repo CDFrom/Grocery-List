f0304e9a5262d077e2982b1a24223bd88526ac7a
Colin From <cdfrom481@gmail.com>
2023-09-04 09:22:12 -0600

    Fill and style Item.jsx

--- src/components/ListItems/Item.jsx
+++ src/components/ListItems/Item.jsx
@@ -1,5 +1,15 @@
+import Button from "../UI/Button/Button";
+import Card from "../UI/Card/Card";
+
 import classes from "./Item.module.css";
 
-const Item = (props) => {};
+const Item = (props) => {
+  return (
+    <Card className={classes["list-item"]}>
+      {props.item}
+      <Button>X</Button>
+    </Card>
+  );
+};
 
 export default Item;
